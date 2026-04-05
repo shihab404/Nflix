11fda9808403bf6bf6cdfd5060d437077421245d
shihab404 <shihab40@proton.me>
2026-04-05 20:24:19 +0600

    resolved conflict

---
+++ NFLIX/src/main/kotlin/com/shihab/NflixMoviesProvider.kt
@@ -0,0 +1,298 @@
+package com.shihab
+
+import com.lagradost.cloudstream3.Episode
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchQuality
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.addDubStatus
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.getDurationFromString
+import com.lagradost.cloudstream3.mainPageOf
+import com.lagradost.cloudstream3.newAnimeSearchResponse
+import com.lagradost.cloudstream3.newEpisode
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
+
+//suspend fun main() {
+//    val providerTester = com.lagradost.cloudstreamtest.ProviderTester(NflixProvider())
+////    providerTester.testAll()
+////    providerTester.testMainPage(verbose = true)
+//    providerTester.testSearch(query = "gun",verbose = true)
+////    providerTester.testLoad("")
+//}
+
+class NflixMoviesProvider : MainAPI() {
+    override var mainUrl = "http://nflix.netscopebd.com"
+    private val apiUrl = "http://10.0.5.3"
+    override var name = "Nflix"
+    override var lang = "bn"
+    override val hasMainPage = true
+    override val hasDownloadSupport = true
+    override val hasQuickSearch = false
+    override val hasChromecastSupport = true
+    override val supportedTypes = setOf(
+        TvType.Movie,
+        TvType.TvSeries,
+        TvType.Anime,
+        TvType.AnimeMovie,
+        TvType.Cartoon,
+        TvType.AsianDrama,
+        TvType.Documentary,
+        TvType.OVA,
+        TvType.Others
+    )
+    override val mainPage = mainPageOf(
+        "80" to "Featured",
+        "6" to "English Movies",
+        "9" to "English & Foreign TV Series",
+        "22" to "Dubbed TV Series",
+        "2" to "Hindi Movies",
+        "5" to "Hindi TV Series",
+        "238" to "Indian TV Show",
+        "7" to "English & Foreign Hindi Dubbed Movies",
+        "8" to "Foreign Language Movies",
+        "3" to "South Indian Dubbed Movies",
+        "4" to "South Indian Movies",
+        "1" to "Animation Movies",
+        "21" to "Anime Series",
+        "85" to "Documentary",
+        "15" to "WWE"
+    )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val json = try {
+            app.get(
+                "$mainApiUrl/api/posts?categoryExact=${request.data}&page=$page&order=desc&limit=10",
+                verify = false,
+                cacheTime = 60
+            )
+            // First try mainApiUrl
+        } catch (_: Exception) {
+            app.get(
+                "$apiUrl/api/posts?categoryExact=${request.data}&page=$page&order=desc&limit=10",
+                verify = false,
+                cacheTime = 60
+            )
+            // Fallback to apiUrl
+        }
+        val home = AppUtils.parseJson<PageData>(json.text).posts.mapNotNull { post ->
+            toSearchResult(post)
+        }
+        return newHomePageResponse(request.name, home, true)
+    }
+
+    private fun toSearchResult(post: Post): SearchResponse? {
+        if (post.type == "singleVideo" || post.type == "series") {
+            return newAnimeSearchResponse(post.title, "$mainUrl/content/${post.id}", TvType.Movie) {
+                this.posterUrl = "$mainApiUrl/uploads/${post.imageSm}"
+                val check = post.title.lowercase()
+                this.quality = getSearchQuality(check)
+                addDubStatus(
+                    dubExist = when {
+                        "dubbed" in check -> true
+                        "dual audio" in check -> true
+                        "multi audio" in check -> true
+                        else -> false
+                    },
+                    subExist = false
+                )
+            }
+        }
+        return null
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val json = try {
+            app.get(
+                "$mainApiUrl/api/posts?searchTerm=$query&order=desc",
+                verify = false,
+                cacheTime = 60
+            )
+            // First try mainApiUrl
+        } catch (_: Exception) {
+            app.get(
+                "$apiUrl/api/posts?searchTerm=$query&order=desc",
+                verify = false,
+                cacheTime = 60
+            )
+            // Fallback to apiUrl
+        }
+        return AppUtils.parseJson<PageData>(json.text).posts.mapNotNull { post ->
+            toSearchResult(post)
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val json = try {
+            app.get(
+                url.replace("$mainUrl/content/", "$mainApiUrl/api/posts/"),
+                verify = false,
+                cacheTime = 60
+            )
+            // First try mainApiUrl
+        } catch (_: Exception) {
+            app.get(
+                url.replace("$mainUrl/content/", "$apiUrl/api/posts/"),
+                verify = false,
+                cacheTime = 60
+            )
+            // Fallback to apiUrl
+        }
+        val urlCheck = json.url.contains(mainApiUrl)
+        val loadData = AppUtils.parseJson<Data>(json.text)
+        val title = loadData.title
+        val poster = "$apiUrl/uploads/${loadData.image}"
+        val description = loadData.metaData
+        val year = selectUntilNonInt(loadData.year)
+
+        if (loadData.type == "singleVideo") {
+            val movieUrl = json.parsed<Movies>().content
+            val link = if(urlCheck) movieUrl else linkToIp(movieUrl)
+            val duration =
+                getDurationFromString(loadData.watchTime)
+            return newMovieLoadResponse(title, url, TvType.Movie, link) {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = description
+                this.duration = duration
+            }
+        } else {
+            val tvData = json.parsed<TvSeries>()
+            val episodesData = mutableListOf<Episode>()
+            var seasonNum = 0
+            tvData.content.forEach { season ->
+                seasonNum++
+                var episodeNum = 0
+                season.episodes.forEach {
+                    episodeNum++
+                    val episodeUrl = it.link
+                    val link = if(urlCheck) episodeUrl else linkToIp(episodeUrl)
+                    episodesData.add(
+                        newEpisode(link){
+                            //this.name = "Episode $episodeNum"
+                            this.episode = episodeNum
+                            this.season = seasonNum
+                        }
+                    )
+                }
+            }
+            return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodesData) {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = description
+            }
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        callback.invoke(
+            newExtractorLink(
+                source = this.name,
+                name = this.name,
+                url = data
+            )
+        )
+        return true
+    }
+
+    /**
+     * Extracts the initial numeric part of a string and returns it as an integer.
+     *
+     * @param string The input string.
+     * @return The initial numeric part as an integer, or `null` if the string doesn't start with a number or is null.
+     */
+    private fun selectUntilNonInt(string: String?): Int? {
+        return string?.let { Regex("^.*?(?=\\D|$)").find(it)?.value?.toIntOrNull() }
+    }
+
+    /**
+     * Determines the search quality based on the presence of specific keywords in the input string.
+     *
+     * @param check The string to check for keywords.
+     * @return The corresponding `SearchQuality` enum value, or `null` if no match is found.
+     */
+    private fun getSearchQuality(check: String?): SearchQuality? {
+        val lowercaseCheck = check?.lowercase()
+        if (lowercaseCheck != null) {
+            return when {
+                lowercaseCheck.contains("webrip") || lowercaseCheck.contains("web-dl") -> SearchQuality.WebRip
+                lowercaseCheck.contains("bluray") -> SearchQuality.BlueRay
+                lowercaseCheck.contains("hdts") || lowercaseCheck.contains("hdcam") || lowercaseCheck.contains(
+                    "hdtc"
+                ) -> SearchQuality.HdCam
+
+                lowercaseCheck.contains("dvd") -> SearchQuality.DVD
+                lowercaseCheck.contains("cam") -> SearchQuality.Cam
+                lowercaseCheck.contains("camrip") || lowercaseCheck.contains("rip") -> SearchQuality.CamRip
+                lowercaseCheck.contains("hdrip") || lowercaseCheck.contains("hd") || lowercaseCheck.contains(
+                    "hdtv"
+                ) -> SearchQuality.HD
+
+                lowercaseCheck.contains("telesync") -> SearchQuality.Telesync
+                lowercaseCheck.contains("telecine") -> SearchQuality.Telecine
+                else -> null
+            }
+        }
+        return null
+    }
+
+    data class PageData(
+        val posts: List<Post>
+    )
+
+    data class Post(
+        val id: Int,
+        val type: String,
+        val imageSm: String,
+        val title: String,
+        val name: String?,
+    )
+
+    data class Data(
+        val type: String,
+        val imageSm: String,
+        val title: String,
+        val image: String,
+        val metaData: String?,
+        val name: String,
+        val quality: String?,
+        val year: String?,
+        val watchTime: String?
+    )
+
+    data class TvSeries(
+        val content: List<Content>,
+    )
+
+    data class Content(
+        val episodes: List<EpisodeData>,
+        val seasonName: String
+    )
+
+    data class EpisodeData(
+        val link: String,
+        val title: String
+    )
+
+    data class Movies(
+        val content: String?
+    )
+
+}
